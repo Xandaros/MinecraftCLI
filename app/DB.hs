ea024fa9fa310e48044fb6d4e5755e4b682422ca
Martin Zeller <mz.bremerhaven@gmail.com>
2018-03-20 16:30:56 +0100

    Add command line

--- app/DB.hs
+++ app/DB.hs
@@ -8,8 +8,30 @@
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
-module DB where
+module DB ( getProfiles
+          , newProfile
+          , deleteProfile
+          , getProfile
+          , updateAccessToken
+          , getClientToken
+          , getServers
+          , newServer
+          , deleteServer
+          , getServer
+          , Profile(..)
+          , profileUsername
+          , profileUuid
+          , profileToken
+          , Server(..)
+          , serverName
+          , serverAddress
+          ) where
+import Control.Lens
+import Control.Monad (void)
+import Control.Monad.IO.Class (liftIO)
 import Data.Text (Text)
+import qualified Data.UUID as UUID
+import qualified Data.UUID.V4 as UUID
 import Database.Persist
 import Database.Persist.Sqlite
 import Database.Persist.TH
@@ -21,11 +43,75 @@ Profile
     username Text
     uuid Text
     token Text
-    active Bool
+    UniqueProfileUsername username
+    deriving Show
+KV
+    key Text
+    value Text
+    UniqueKey key
+    deriving Show
+Server
+    name Text
+    address Text
+    UniqueServerName name
+    deriving Show
 |]
 
-getActiveProfiles :: IO [Profile]
-getActiveProfiles = runSqlite "MinecraftCLI.db" $ do
-    runMigration migrateAll
+database :: Text
+database = "MinecraftCLI.db"
 
-    fmap entityVal <$> selectList [ProfileActive ==. True] []
+getClientToken :: IO Text
+getClientToken = runSqlite database $ do
+    runMigration migrateAll
+    tokenm <- getBy (UniqueKey "clientToken")
+    case tokenm of
+      Just token -> pure $ entityVal token ^. kVValue
+      Nothing -> do
+          token <- UUID.toText <$> liftIO UUID.nextRandom
+          _ <- insert (KV "clientToken" token)
+          pure token
+
+getProfiles :: IO [Profile]
+getProfiles = runSqlite database $ do
+    runMigration migrateAll
+    fmap entityVal <$> selectList [] []
+
+newProfile :: Profile -> IO ()
+newProfile prof = runSqlite database $ do
+    runMigration migrateAll
+    void $ insert prof
+
+deleteProfile :: Text -> IO ()
+deleteProfile username = runSqlite database $ do
+    runMigration migrateAll
+    deleteBy (UniqueProfileUsername username)
+
+getProfile :: Text -> IO (Maybe Profile)
+getProfile username = runSqlite database $ do
+    runMigration migrateAll
+    fmap entityVal <$> getBy (UniqueProfileUsername username)
+
+updateAccessToken :: Text -> Text -> IO ()
+updateAccessToken username accessToken = runSqlite database $ do
+    runMigration migrateAll
+    updateWhere [ProfileUsername ==. username] [ProfileToken =. accessToken]
+
+getServers :: IO [Server]
+getServers = runSqlite database $ do
+    runMigration migrateAll
+    fmap entityVal <$> selectList [] []
+
+newServer :: Server -> IO ()
+newServer server = runSqlite database $ do
+    runMigration migrateAll
+    void $ insert server
+
+deleteServer :: Text -> IO ()
+deleteServer name = runSqlite database $ do
+    runMigration migrateAll
+    deleteBy (UniqueServerName name)
+
+getServer :: Text -> IO (Maybe Server)
+getServer name = runSqlite database $ do
+    runMigration migrateAll
+    fmap entityVal <$> getBy (UniqueServerName name)
--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,7 @@ import           Control.Concurrent (threadDelay, forkIO)
 import           Control.Concurrent.Async
 import           Control.Concurrent.STM
 import           Control.Lens
-import           Control.Monad (void, when, forever)
+import           Control.Monad (void, when, forever, forM_)
 import           Control.Monad.Fix (MonadFix)
 import           Control.Monad.IO.Class
 import           Data.IORef
@@ -20,8 +20,8 @@ import           Data.List (isPrefixOf)
 import qualified Data.Text as T
 import           Data.Text (Text)
 import qualified Data.Text.Encoding as TE
-import qualified Data.Text.IO as TIO
 import           Safe
+import           System.Console.Haskeline
 import           System.Exit (exitSuccess)
 import           Reflex hiding (performEvent_)
 import           Reflex.Host.App
@@ -29,9 +29,14 @@ import           Reflex.Host.App
 import Network.Protocol.Minecraft
 import Network.Protocol.Minecraft.Packet
 import Network.Protocol.Minecraft.Types
+import qualified Network.Protocol.Minecraft.Yggdrasil as Yggdrasil
+import Network.Protocol.Minecraft.Yggdrasil ()
 
 import Commands
-import DB
+import DB ( Profile(..), profileUsername, profileUuid, profileToken
+          , Server(..), serverName, serverAddress
+          )
+import qualified DB
 import Inventory
 
 import Debug.Trace
@@ -39,25 +44,23 @@ import Debug.Trace
 botName :: String
 botName = "Yotanido"
 
-minecraftThread :: TChan CBPacket -> TChan [SBPacket] -> IORef Bool -> Profile -> IO ()
-minecraftThread inbound outbound shutdown profile = do
-    putStrLn "Connecting"
-
-    void $ connect "158.69.23.101" Nothing $ do
-        liftIO $ putStrLn "Sending handshake"
+minecraftThread :: TChan CBPacket -> TChan [SBPacket] -> IORef Bool -> Profile -> Server -> (String -> IO ()) -> IO ()
+minecraftThread inbound outbound shutdown profile server printfunc = do
+    void $ connect (T.unpack $ server ^. serverAddress) Nothing $ do
+        liftIO $ printfunc "Sending handshake"
         handshake
-        liftIO $ putStrLn "Handshake sent"
+        liftIO $ printfunc "Handshake sent"
 
         loginSucc <- login (profile ^. profileUsername) (profile ^. profileUuid) (profile ^. profileToken)
 
         case loginSucc of
           Left err -> liftIO $ putStrLn err
           Right _ -> liftIO $ do
-              putStrLn "Logged in"
+              printfunc "Logged in"
 
         sendPacket $ SBClientSettingsPayload "en_GB" 1 0 True 0x7F 1
         sendPacket $ SBClientStatusPayload 0
-        sendPacket $ SBChatMessage $ SBChatMessagePayload ("Beep. Boop. I'm a bot. Type \"" <> T.pack botName ^. network <> ": help\" for more information")
+        sendPacket $ SBChatMessage $ SBChatMessagePayload ("Beep. Boop. I'm a bot. Type \"" <> profile ^. profileUsername . network <> ": help\" for more information")
         sendPacket $ SBChatMessagePayload "/afk"
 
         whileM (liftIO $ not <$> readIORef shutdown) $ do
@@ -71,16 +74,16 @@ minecraftThread inbound outbound shutdown profile = do
                           let response = SBKeepAlivePayload $ keepAlive ^. keepAliveId
                           sendPacket response
                       CBPlayerPositionAndLook positionAndLook -> do
-                          liftIO $ putStrLn "Position and look"
+                          liftIO $ printfunc "Position and look"
                           sendPacket $ SBTeleportConfirmPayload $ positionAndLook ^. posLookID
                           sendPacket $ SBPlayerPositionAndLookPayload (positionAndLook^.x) (positionAndLook^.y) (positionAndLook^.z) (positionAndLook^.yaw) (positionAndLook^.pitch) True
                           liftIO . atomically $ writeTChan inbound packet
                       CBDisconnectPlay dc -> do
-                          liftIO $ putStrLn $ "Disconnected: " ++ T.unpack (dc ^. reason . from network)
+                          liftIO $ printfunc $ "Disconnected: " ++ T.unpack (dc ^. reason . from network)
                       _ -> liftIO . atomically $ writeTChan inbound packet
                   Nothing -> liftIO $ do
                       writeIORef shutdown True
-                      putStrLn "Connection closed" >> exitSuccess
+                      printfunc "Connection closed" >> exitSuccess
             dataToSend <- liftIO . atomically $ tryReadTChan outbound
             case dataToSend of
               Just dataToSend -> sequence_ $ sendPacket <$> dataToSend
@@ -89,25 +92,25 @@ minecraftThread inbound outbound shutdown profile = do
                 threadDelay (floor $ ((1/21) :: Double) * 1000000)
 
 
-frpThread :: TChan CBPacket -> TChan [SBPacket] -> IORef Bool -> Profile -> IO ()
-frpThread inbound outbound shutdown _profile = runSpiderHost $ hostApp app
+frpThread :: TChan CBPacket -> TChan [SBPacket] -> IORef Bool -> Profile -> (String -> IO ()) -> IO ()
+frpThread inbound outbound shutdown profile printfunc = runSpiderHost $ hostApp app
     where app :: MonadAppHost t m => m ()
           app = do
               (inputEvent, inputFire) <- newExternalEvent
               (tickEvent, tickFire) <- newExternalEvent
               void . liftIO . forkIO . forever $ threadDelay (floor (1/20 * 1000000 :: Double)) >>= tickFire
               void . liftIO . forkIO . forever $ atomically (readTChan inbound) >>= inputFire
-              (outEvent, printEvent, shutdownEvent) <- minecraftBot inputEvent tickEvent
+              (outEvent, printEvent, shutdownEvent) <- minecraftBot (profile ^. profileUsername) inputEvent tickEvent
               performEvent_ $ fmap (liftIO . atomically . writeTChan outbound) outEvent
-              performEvent_ $ fmap (sequence_ . fmap liftIO . fmap TIO.putStrLn . NE.toList) printEvent
+              performEvent_ $ fmap (sequence_ . fmap liftIO . fmap (printfunc . T.unpack) . NE.toList) printEvent
               performEvent_ $ fmap (const . liftIO $ threadDelay 10000 >> writeIORef shutdown True >> exitSuccess) shutdownEvent
               pure ()
 
-minecraftBot :: (MonadHold t m, MonadFix m, Reflex t) => Event t CBPacket -> Event t ()
+minecraftBot :: (MonadHold t m, MonadFix m, Reflex t) => Text -> Event t CBPacket -> Event t ()
              -> m (Event t [SBPacket], Event t (NonEmpty Text), Event t ())
-minecraftBot inbound tick = do
+minecraftBot botName inbound tick = do
     let chatMessages = chatMessageE inbound
-        commands = commandE chatMessages
+        commands = commandE botName chatMessages
     -- ticks <- zipListWithEvent (\a _ -> a) [1..] tick
     -- seconds <- zipListWithEvent (\a _ -> a) ([1..] :: [Integer]) $ ffilter ((==0) . (`mod` (20 :: Integer))) ticks
     playerPos <- playerPositionD inbound (tpCommandE commands)
@@ -152,15 +155,14 @@ chatMessageE inbound = fforMaybe inbound $ \case
     CBChatMessage (CBChatMessagePayload text _) -> fmap decodeChat . chatToString $ text ^. from network
     _ -> Nothing
 
-commandE :: Reflex t => Event t ChatMsg -> Event t ChatCommand
-commandE = fmapMaybe (chatToCommand (T.pack botName))
+commandE :: Reflex t => Text -> Event t ChatMsg -> Event t ChatCommand
+commandE botName = fmapMaybe (chatToCommand botName)
 
 chatToString :: Text -> Maybe String
 chatToString c = T.unpack . chatToText . canonicalizeChatComponent <$> decode (BSL.fromStrict $ TE.encodeUtf8 c)
 
 chatToCommand :: Text -> ChatMsg -> Maybe ChatCommand
 chatToCommand botName msg = do
-    traceM ("Msg: " ++ show msg)
     stripped <- T.stripPrefix (botName <> ": ") (msg ^. message)
     let split = T.words stripped
     cmd <- headMay split
@@ -182,16 +184,109 @@ stripParagraphs [] = []
 stripParagraphs ('§':_:xs) = stripParagraphs xs
 stripParagraphs (x:xs) = x:stripParagraphs xs
 
+profileInput :: [String] -> InputT IO ()
+profileInput ("help":_) = do
+    outputStrLn "Available subcommands:"
+    outputStrLn "help              - Display this help"
+    outputStrLn "list              - List available profiles"
+    outputStrLn "new <login>       - Create a new profile"
+    outputStrLn "delete <username> - Delete an existing profile"
+profileInput ("list":_) = do
+    profiles <- liftIO DB.getProfiles
+    forM_ profiles $ \prof -> do
+        outputStrLn . T.unpack $ prof ^. profileUsername
+profileInput ("new":login:_) = do
+    passwordm <- getPassword Nothing "Password: "
+    case passwordm of
+        Nothing -> pure ()
+        Just password -> do
+            clientToken <- liftIO DB.getClientToken
+            responsem <- liftIO $ Yggdrasil.authenticate (Yggdrasil.AuthenticationRequest Yggdrasil.defaultAgent
+                                                                                          (T.pack login)
+                                                                                          (T.pack $ password)
+                                                                                          clientToken
+                                                                                          False)
+            case responsem of
+                Nothing -> outputStrLn "Unable to create profile (wrong login/password?)"
+                Just response -> do
+                    let profile = response ^. Yggdrasil.selectedProfile
+                        accessToken = response ^. Yggdrasil.accessToken :: Text
+                    liftIO $ DB.newProfile (Profile (profile ^. Yggdrasil.name) (profile ^. Yggdrasil.id) accessToken)
+                    outputStrLn "Profile created"
+profileInput ("delete":username:_) = do
+    liftIO $ DB.deleteProfile (T.pack username)
+    outputStrLn "Profile deleted"
+profileInput _ = profileInput ["help"]
+
+serverInput :: [String] -> InputT IO ()
+serverInput ("help":_) = do
+    outputStrLn "Available subcommands:"
+    outputStrLn "help                 - Display this help"
+    outputStrLn "list                 - List available servers"
+    outputStrLn "new <name> <address> - Create a new server"
+    outputStrLn "delete <name>        - Delete an existing server"
+serverInput ("list":_) = do
+    servers <- liftIO DB.getServers
+    forM_ servers $ \server -> do
+        outputStrLn . T.unpack $ server ^. serverName
+serverInput ("new":name:address:_) = do
+    liftIO $ DB.newServer (Server (T.pack name) (T.pack address))
+    outputStrLn "Server created"
+serverInput ("delete":name:_) = do
+    liftIO $ DB.deleteServer (T.pack name)
+    outputStrLn "Server deleted"
+serverInput _ = serverInput ["help"]
+
+connectInput :: [String] -> InputT IO ()
+connectInput (profile:server:_) = do
+    outputStrLn "Connecting to server"
+    printfunc <- getExternalPrint
+    liftIO $ do
+        profilem <- DB.getProfile (T.pack profile)
+        serverm <- DB.getServer (T.pack server)
+        case profilem of
+          Nothing -> pure ()
+          Just profile ->
+              case serverm of
+                Nothing -> pure ()
+                Just server -> do
+                    clientToken <- DB.getClientToken
+                    responsem <- Yggdrasil.refresh (Yggdrasil.RefreshRequest (profile ^. profileToken)
+                                                                             clientToken
+                                                                             False)
+                    case responsem of
+                      Nothing -> printfunc "Could not refresh token - try recreating the profile"
+                      Just response -> do
+                          DB.updateAccessToken (profile ^. profileUsername) (response ^. Yggdrasil.accessToken)
+                          let newprofile = profile & profileToken .~ (response ^. Yggdrasil.accessToken)
+                          inbound <- atomically $ newTChan
+                          outbound <- atomically $ newTChan
+                          shutdown <- newIORef False
+                          async (minecraftThread inbound outbound shutdown newprofile server printfunc) >>= link
+                          async (frpThread inbound outbound shutdown newprofile printfunc) >>= link
+connectInput _ = outputStrLn "You need to provide a profile and a server"
+
 main :: IO ()
 main = do
-    profiles <- getActiveProfiles
-    when (null profiles) $ do
-        putStrLn "No profiles found"
-        exitSuccess
-    inbound <- atomically $ newTChan
-    outbound <- atomically $ newTChan
-    shutdown <- newIORef False
-    concurrently_ (minecraftThread inbound outbound shutdown (head profiles)) (frpThread inbound outbound shutdown (head profiles))
+    runInputT defaultSettings $ do
+        loop
+        where loop :: InputT IO ()
+              loop = do
+                  minput <- getInputLine "% "
+                  case words <$> minput of
+                    Nothing -> pure ()
+                    Just ("quit":_) -> pure ()
+                    Just ("profiles":args) -> profileInput args >> loop
+                    Just ("servers":args) -> serverInput args >> loop
+                    Just ("connect":args) -> connectInput args >> loop
+                    Just input -> do
+                        outputStrLn ("Unrecognised command: " ++ unwords input)
+                        outputStrLn ("Available commands:")
+                        outputStrLn ("quit                       - Close MinecraftCLI")
+                        outputStrLn ("profiles                   - Manage profiles")
+                        outputStrLn ("servers                    - Manage servers")
+                        outputStrLn ("connect <profile> <server> - Connect a profile to a server")
+                        loop
 
 mkPPAL :: (Double, Double, Double) -> SBPacket
 mkPPAL (x, y, z) = SBPlayerPositionAndLook $ SBPlayerPositionAndLookPayload (x ^. network) (y ^. network) (z ^. network) 0 0 True
